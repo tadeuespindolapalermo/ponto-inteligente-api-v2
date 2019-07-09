859416337076e65aa6ab489d32d61d4bfb49725f
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2019-07-09 01:54:51 -0300

    Commit inicial dos arquivos do projeto.

---
+++ src/main/java/br/com/tadeudeveloper/pontointeligente/api/v2/PontoInteligenteV2Application.java
@@ -0,0 +1,13 @@
+package br.com.tadeudeveloper.pontointeligente.api.v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligenteV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteV2Application.class, args);
+	}
+
+}
