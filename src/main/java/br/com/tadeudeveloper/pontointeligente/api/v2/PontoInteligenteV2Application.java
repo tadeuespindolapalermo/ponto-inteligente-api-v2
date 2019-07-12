d5906c727893e06a9cdc2b2b608ee51a68b0094c
Tadeu Espíndola Palermo <tepalermo@stefanini.com>
2019-07-12 14:15:38 -0300

    Adicionado EhCache na aplicação.

--- src/main/java/br/com/tadeudeveloper/pontointeligente/api/v2/PontoInteligenteV2Application.java
+++ src/main/java/br/com/tadeudeveloper/pontointeligente/api/v2/PontoInteligenteV2Application.java
@@ -2,8 +2,10 @@ package br.com.tadeudeveloper.pontointeligente.api.v2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PontoInteligenteV2Application {
 
 	public static void main(String[] args) {
